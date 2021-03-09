dfb34109e8742f45b2140a77cf435243a4f497a3
Maximiliano <maximilianodacruz@gmail.com>
2021-03-09 09:39:44 -0300

    add example

--- cypress/integration/services/produtos/requests/postProdutos.request.js
+++ cypress/integration/services/produtos/requests/postProdutos.request.js
@@ -1,8 +1,11 @@
 /// <reference types="cypress" />
 
 const payloadAddProduto = require('../payloads/add-produto.payload.json')
+var faker = require('faker'); 
 
 function adicionar(auth) {
+    payloadAddProduto.nome = `Maquina de lava ${faker.name.findName()}`
+    payloadAddProduto.descricao = `project-${faker.random.uuid()}`
     return cy.request({
         method: "POST",
         url: "produtos",
